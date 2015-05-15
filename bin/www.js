fcec741432fcd8f5ec0f3d6f7abc9a63c4aa4082
Jan Beckmann <king-jan1999@hotmail.de>
2015-05-15 14:17:05 +0200

    Of Fix

--- bin/www.js
+++ bin/www.js
@@ -3,8 +3,12 @@
  */
 var app = require('../app'); //Require our app
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8000);
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
-var server = app.listen(app.get('port'), function() {
+app.set('port', server_port);
+app.listen(server_ip_address);
+
+var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
