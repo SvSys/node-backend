eb9268ef482db94eeb4f7517b8b1fab21fccab2c
Jan Beckmann <king-jan1999@hotmail.de>
2015-05-16 12:30:01 +0200

    Adress log fix

--- bin/www.js
+++ bin/www.js
@@ -8,6 +8,5 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 app.set('port', server_port);
 
 var server = app.listen(app.get('port'),server_ip_address, function () {
-    console.log('Express server listening on ip ' + server.address().);
-    console.log('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on adress %j',server.address());
 });
