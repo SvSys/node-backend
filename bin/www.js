a800e0ca5c285e5c4ec81ccba42ad4d85e698d13
Jan Beckmann <king-jan1999@hotmail.de>
2015-05-16 12:28:49 +0200

    Ip Fix

--- bin/www.js
+++ bin/www.js
@@ -5,10 +5,9 @@ var app = require('../app'); //Require our app
 
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
-
 app.set('port', server_port);
-app.listen(server_ip_address);
 
-var server = app.listen(app.get('port'), function () {
+var server = app.listen(app.get('port'),server_ip_address, function () {
+    console.log('Express server listening on ip ' + server.address().);
     console.log('Express server listening on port ' + server.address().port);
 });
